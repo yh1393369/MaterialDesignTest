ca7622a6724cc81f37daaae3091febdc48c7cb34
Rookie <170018701@qq.com>
2017-04-12 16:11:07 +0800

    DrawerLayout+NavigationView

--- app/src/main/java/com/rookie/www/materialdesigntest/MainActivity.java
+++ app/src/main/java/com/rookie/www/materialdesigntest/MainActivity.java
@@ -1,22 +1,60 @@
 package com.rookie.www.materialdesigntest;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import java.lang.reflect.Method;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar tbMain;
+    private DrawerLayout dlMain;
+    private NavigationView nvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar tbMain = (Toolbar) findViewById(R.id.tbMain);
+        tbMain = (Toolbar) findViewById(R.id.tbMain);
         setSupportActionBar(tbMain);
+        dlMain = (DrawerLayout) findViewById(R.id.dlMain);
+        nvMain = (NavigationView) findViewById(R.id.nvMain);
+        View headerView = nvMain.inflateHeaderView(R.layout.nav_header_main);
+        CircleImageView civNavHeaderMain = (CircleImageView) headerView.findViewById(R.id.civNavHeaderMain);
+        civNavHeaderMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "显示图片", Toast.LENGTH_SHORT).show();
+                dlMain.closeDrawers();
+            }
+        });
+        nvMain.setItemIconTintList(null);
+        nvMain.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(MainActivity.this, "选择了"+item.getTitle(), Toast.LENGTH_SHORT).show();
+                dlMain.closeDrawers();
+                return false;
+            }
+        });
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_more);
+        }
     }
 
     @Override
@@ -34,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.menuItemExit :
                 finish();
+                break;
+            case android.R.id.home :
+                dlMain.openDrawer(GravityCompat.START, true);
                 break;
         }
         return true;
